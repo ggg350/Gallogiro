9b40d14e781c140bfce39ecd690dd0ecfab3f800
ggg350 <126991529+ggg350@users.noreply.github.com>
2023-04-28 22:37:14 -0700

    Update Chamba.sql

--- Chamba.sql
+++ Chamba.sql
@@ -18,13 +18,13 @@ create table Almacen
 (
 ID_Almacen int not null ,
 ID_Zona int not null,
-ID_Municipio int not null,
+
 )
 
 create table Cedis 
 (
 ID_Cedis int not null,
---ID_Almacen int foreign key references Almacen (ID_Almacen),
+ID_Almacen int not null,
 Nombre nvarchar (50) null,
 Direccion nvarchar (100) not null,
 Telefono varchar (20),
@@ -32,7 +32,7 @@ Telefono varchar (20),
 create table Sucursal
 (
 ID_Sucursal int not null,
---ID_Almacen int foreign key references Almacen (ID_Almacen),
+ID_Almacen int not null, 
 Nombre nvarchar (50) null,
 Direccion nvarchar (100) not null,
 Telefono varchar (20),
@@ -49,24 +49,24 @@ create table Familia
 ID_Familia int not null,
 FamiliaNombre nvarchar (100) not null, 
 FamiliaDescripcion nvarchar (100) not null, 
-
-)
+) 
 create table Producto 
 (
 UPC int not null, 
---ID_Familia int foreign key references Familia (ID_Familia),
---ID_Componenteactivo int foreign key references Componenteactivo (ID_Componenteactivo),
+ID_Familia int not null,
+ID_Componenteactivo int not null,
 Nombre nvarchar (50) not null,
 Descripcion nvarchar (100) null, 
 PrecioVenta money not null default 0.0,
-
 )
+
 create table Inventario 
 (
---ID_Almacen int foreign key references Almacen (ID_Almacen),
---UPC int foreign key references Producto (UPC),
+ID_Almacen int not null,
+UPC int not null,
 Existencia int not null default 0,
 )
+
 create table Proveedor
 (
 ID_Proveedor int not null, 
@@ -77,15 +77,15 @@ Telefono varchar (20),
 )
 create table CompraAProveedor
 (
---UPC int foreign key references Producto (UPC),
---ID_Proveedor int foreign key references Proveedor (ID_Proveedor),
+UPC int not null,
+ID_Proveedor int not null,
 Costo money not null default 0.0,
 Cantidad int not null default 0,
-)
+) 
 create table Detalle 
 (
---ID_Pedido int not null identity primary key,
---UPC int foreign key references Producto (UPC),
+ID_Pedido int not null,
+UPC int not null,
 Cantidad int not null,
 PrecioFinal money not null default 0.0,
 )
@@ -120,9 +120,9 @@ Nombre nvarchar(50) not null,
 create table Entrega 
 (
 ID_Entrega int not null , 
---ID_Almacen int foreign key references Almacen (ID_Almacen),
---ID_Pedido int foreign key references Pedido (ID_Pedido),
---ID_Tipodeentrega int foreign key references Tipodeentrega (ID_Tipodeentrega),
+ID_Almacen int not null,
+ID_Pedido int not null,
+ID_Tipodeentrega int not null,
 Direcciondeentrega nvarchar (100) not null,
 Nombredestinatario nvarchar (50) not null,
 Estatusdeentrega nvarchar (30) not null,
@@ -131,17 +131,17 @@ Recibio nvarchar (30) null,
 create table Pedido 
 (
 ID_Pedido int not null, 
---ID_Entrega int foreign key references Entrega (ID_Entrega),
---ID_Cliente int foreign key references Cliente (ID_Cliente),
---ID_Formatodepago int foreign key references Formatodepago (ID_Formatodepago),
---ID_Sucursal int foreign key references Sucursal (ID_Sucursal),
+ID_Entrega int not null,
+ID_Cliente int not null,
+ID_Formatodepago int not null,
+ID_Sucursal int not null,
 Fecha date,
 Costototal int not null,
 )
 create table Empleado 
 (
 ID_Empleado int not null,
---ID_Almacen int foreign key references Almacen (ID_Almacen),
+ID_Almacen int not null,
 Nombre nvarchar (50) not null,
 CURP nvarchar (50) not null,
 RFC nvarchar (50) not null,
@@ -151,33 +151,51 @@ Telefono varchar (20) null,
 go
 --------------------PRIMARYS KEYS------------------------------------------------------------------------------------------
 Alter table Empleado add constraint PK_ID_Empleado primary key (ID_Empleado)
-Alter table Tipodeentrega add constraint PK_ID_Tipodeentrega primary key (ID_Tipodeentrega)
 Alter table Familia add constraint PK_ID_Familia primary key (ID_Familia)
 Alter table Componenteactivo add constraint PK_ID_Componenteactivo primary key (ID_Componenteactivo)
-Alter table Pedido add constraint PK_ID_Pedido primary key (ID_Pedido)
-Alter table Entrega add constraint PK_ID_Entrega primary key (ID_Entrega)
 Alter table Tipodecliente add constraint PK_ID_Tipodecliente primary key (ID_Tipodecliente)
 Alter table Cliente add constraint PK_ID_Cliente primary key (ID_Cliente)
 Alter table Formatodepago add constraint PK_ID_Formatodepago primary key (ID_Formatodepago)
-Alter table Cedis add constraint PK_ID_Cedis primary key (ID_Cedis)
 Alter table Proveedor add constraint PK_ID_Proveedor primary key (ID_Proveedor)
-Alter table Sucursal add constraint PK_ID_Sucursal primary key (ID_Sucursal)
 Alter table Producto add constraint PK_UPC primary key (UPC)
-
-
-
-
---pk
 Alter table Zona add constraint pk_ID_Zona primary key (ID_Zona)
-Alter table Municipio add constraint pk_Municipio_ID_Zona primary key (ID_Zona,ID_Municipio)
+Alter table Sucursal add constraint PK_ID_Sucursal primary key (ID_Sucursal)
+Alter table Cedis add constraint PK_ID_Cedis primary key (ID_Cedis)
 Alter table Almacen add constraint pk_Almacen primary key (ID_Almacen)
+Alter table Tipodeentrega add constraint PK_ID_Tipodeentrega primary key (ID_Tipodeentrega)
+Alter table Entrega add constraint PK_ID_Entrega primary key (ID_Entrega)
+Alter table Pedido add constraint pk_Pedido primary key (ID_Pedido)
+--compuestas
+go
+Alter table Municipio add constraint pk_Municipio_ID_Zona primary key (ID_Zona,ID_Municipio)
+Alter table Inventario add constraint pk_Inventario_UPC primary key (ID_Almacen,UPC)
 --fk
+go
 Alter table Municipio add constraint fk_Municipio foreign key (ID_Zona) references Zona (ID_Zona)
-Alter table Almacen add constraint fk_Almacen foreign key (ID_Zona,ID_Municipio) references Municipio (ID_Zona,ID_Municipio)
+Alter table Almacen add constraint fk_Almacen foreign key (ID_Zona) references Zona (ID_Zona)
+Alter table Empleado add constraint fk_Empleado foreign key (ID_Almacen) references Almacen (ID_Almacen)
+Alter table Cedis add constraint fk_CedAlmacen foreign key (ID_Almacen) references Almacen (ID_Almacen)
+Alter table Sucursal add constraint fk_SucAlmacen foreign key (ID_Almacen) references Almacen (ID_Almacen)
+Alter table Inventario add constraint fk_InvAlmacen foreign key (ID_Almacen) references Almacen (ID_Almacen)
+Alter table Inventario add constraint fk_InvUPC foreign key (UPC) references Producto (UPC)
+Alter table Producto add constraint fk_FanmiliaProducto foreign key (ID_Familia) references Familia (ID_Familia)
+Alter table Producto add constraint fk_ComponenteActivoProducto foreign key (ID_Componenteactivo) references Componenteactivo (ID_Componenteactivo)
+Alter table CompraAProveedor add constraint fk_UPCProveedor foreign key (UPC) references Producto (UPC)
+Alter table CompraAProveedor add constraint fk_Proovedor foreign key (ID_Proveedor) references Proveedor (ID_Proveedor)
 
-
-
-
-select * from Almacen
+go
+Alter table Entrega add constraint fk_EntregaAlmacen foreign key (ID_Almacen) references Almacen (ID_Almacen)
+Alter table Entrega add constraint fk_EntregadePedido foreign key (ID_Pedido) references Pedido (ID_Pedido)
+Alter table Entrega add constraint fk_Tipodeentrega foreign key (ID_Tipodeentrega) references Tipodeentrega (ID_Tipodeentrega)
+go
+Alter table Pedido add constraint fk_EntregaPedido foreign key (ID_Entrega) references Entrega (ID_Entrega)
+Alter table Pedido add constraint fk_ClientePedido foreign key (ID_Cliente) references Cliente (ID_Cliente)
+Alter table Pedido add constraint fk_SucursalPedido foreign key (ID_Sucursal) references Sucursal (ID_Sucursal)
+Alter table Pedido add constraint fk_FormatoPedido foreign key (ID_Formatodepago) references Formatodepago (ID_Formatodepago)
+go
+Alter table Detalle add constraint fk_DetallePedido foreign key (ID_Pedido) references pedido (ID_Pedido)
+Alter table Detalle add constraint fk_DetalleUPC foreign key (UPC) references Producto (UPC)
+Alter table Detalle add constraint pk_DetallePedido_UPC primary key (ID_Pedido,UPC)
 
 --Comando para borrar tablas ---> drop table nombre
+
